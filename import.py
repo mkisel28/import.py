c75c9d8d7609788fbb48d5d97791f086a902b08e
Maksim Kisel <mkisel28@gmail.com>
2021-11-16 17:33:25 +0300

    no

---
+++ import.py
@@ -0,0 +1,11 @@
+import pymorphy2
+from collections import defaultdict
+import re
+import json
+import pymorphy2
+#
+# morph = pymorphy2.MorphAnalyzer()
+# z=morph.parse('они')[0].normal_form
+# print(z)
+z  = ('12 345')
+print(z)
